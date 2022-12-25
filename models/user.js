987ec72f1d38cc87bcb34e5541f438ebed00d17d
Imran-2022 <mdimranulhaque.202@gmail.com>
2022-12-25 17:18:56 +0600

    user model for auth

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose')
+
+const Joi = require('joi')
+
+const jwt = require('jsonwebtoken')
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        maxLength: 100,
+        minLength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 255,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 1024
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, { timestamps: true }
+
+)
+
+userSchema.methods.generateJWT = function(){
+    const token = jwt.sign({
+        _id:this._id,
+        email:this.email,
+        role:this.role,
+        name:this.name,
+    },process.env.JWT_SECRET,{expiresIn:"7d"})
+    return token;
+}
+
+const validateUser=user=>{
+    const schema = Joi.object({
+        name:Joi.string().min(3).max(100).required(),
+        email:Joi.string().min(5).max(255).required(),
+        password:Joi.string().min(5).max(255).required(),
+    })
+    return schema.validate(user)
+}
+
+module.exports.User=model('User',userSchema);
+module.exports.validate=validateUser;

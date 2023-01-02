3b3faa88870f236d709956fe1198caff199f1e8a
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-01-02 15:11:18 +0600

    host database to mongodb and connect

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv/config')
 const app =require('./app')
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGODB_LOCAL_URL)
+const DB=process.env.MONGODB_SERVER.replace('<PASSWORD>',process.env.DB_PASSWORD)
+mongoose.connect(DB)
     .then(() => console.log("connected to mongodb"))
     .catch(err => console.error('Mongodb Connection Failed'))
 
@@ -11,3 +12,19 @@ const port = process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`app running on port ${port}`);
 })
+
+// -----------------------------------
+
+// require('dotenv/config')
+// const app =require('./app')
+// const mongoose = require('mongoose');
+// mongoose.set('strictQuery', true)
+// mongoose.connect(process.env.MONGODB_LOCAL_URL)
+//     .then(() => console.log("connected to mongodb"))
+//     .catch(err => console.error('Mongodb Connection Failed'))
+
+// const port = process.env.PORT || 3001;
+
+// app.listen(port,()=>{
+//     console.log(`app running on port ${port}`);
+// })
